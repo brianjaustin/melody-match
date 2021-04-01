5c267728175e5cdad67ffacc43ddfb2829b62353
Emma Reed <reed.em@husky.neu.edu>
2021-03-31 23:19:20 -0400

    porting old code from spotifriend experiement

--- web-ui/melody-match/src/App.js
+++ web-ui/melody-match/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { Tracks } from "./Tracks/Tracks";
+import { useState, useEffect } from "react";
+import { Register } from "./Login/Register";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// Get the hash of the url
+const hash = window.location.hash
+  .substring(1)
+  .split("&")
+  .reduce(function (initial, item) {
+    if (item) {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+    }
+    return initial;
+  }, {});
+window.location.hash = "";
 
 function App() {
+  let _token = hash.access_token;
+  const [state, setState] = useState({
+    loggedIn: false,
+    token: false,
+    tracks: false,
+  });
+  useEffect(() => {
+    if (_token) {
+      setState({ loggedIn: false, token: _token, tracks: false });
+    }
+  }, [_token]);
+
+  function getTopTracks() {
+  }
+
+  let body = <Register spotify={state.token} submit={getTopTracks}></Register>;
+  if (state.tracks) {
+    body = <p>{JSON.stringify(state.tracks.items)}</p>;
+    body = <Tracks tracks={state.tracks.items} />;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Melody Matches</header>
+      {body}
     </div>
   );
 }
---
+++ web-ui/melody-match/src/Login/Register.js
@@ -0,0 +1,78 @@
+import "./Register.scss";
+import { Form, Button, Container} from "react-bootstrap";
+import React, {useEffect, useState} from 'react';
+
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+// Replace with your app's client ID, redirect URI and desired scopes
+const clientId = process.env.REACT_APP_CLIENT_ID;
+const redirectUri = "http%3A%2F%2Flocalhost%3A3000";
+const scopes = ["user-top-read"];
+
+
+export function Register({spotify, submit}) {
+
+
+  let spotify_component = (
+    <Button
+      href={`${authEndpoint}?response_type=token&client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+        "%20"
+      )}&show_dialog=true`}
+      className="spotify"
+    >
+      Login to Spotify
+    </Button>
+  );
+
+  if (spotify){
+    console.log(spotify)
+    spotify_component = (
+      <div>
+        {" "}
+        <Button
+          href={`${authEndpoint}?response_type=token&client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+            "%20"
+          )}&show_dialog=true`}
+          className="spotify"
+          disabled
+        >
+          Login to Spotify
+        </Button>
+        <p>Token has been collected</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="Register">
+      <Container>
+        <h2>Register a New User</h2>
+        <Container>
+          <p>Step 1: Allow Spotify Access.</p>
+          {spotify_component}
+        </Container>
+        <hr />
+        <Container>
+          <p>Step 2: Fill in Personal Information</p>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>User Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter user name" />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                Email will be used to connect you with other users.
+              </Form.Text>
+            </Form.Group>
+
+            <Button variant="primary" onClick={submit}>
+              Submit
+            </Button>
+          </Form>
+        </Container>
+      </Container>
+    </div>
+  );
+}
