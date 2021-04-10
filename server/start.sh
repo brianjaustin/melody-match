e80bd775e8d2e2ae1b5de16e7cb08ec4f8263bb3
Brian Austin <brianjaustin@gmail.com>
2021-04-10 13:18:22 -0400

    Initial build/start scripts

---
+++ server/start.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# This script was copied from hw06-08
+export SECRET_KEY_BASE=W68eso5YQOlbtvSNUR50N/HDWj6IaEhAwMR3LtzuBEQAefwYVbX84bvoTA7XtiGi
+export MIX_ENV=prod
+export PORT=4800
+
+echo "Stopping old copy of app, if any..."
+
+_build/prod/rel/melody_match/bin/melody_match stop || true
+
+CFGD=$(readlink -f ~/.config/melody_match)
+
+if [ ! -e "$CFGD/base" ]; then
+    echo "run build first"
+    exit 1
+fi
+
+SECRET_KEY_BASE=$(cat "$CFGD/base")
+DATABASE_URL=$(cat "$CFGD/postgres")
+export SECRET_KEY_BASE
+export DATABASE_URL
+
+echo "Starting app..."
+
+_build/prod/rel/melody_match/bin/melody_match start
